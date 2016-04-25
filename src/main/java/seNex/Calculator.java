2e832708d08e1e0e1b285045b67c8f7dc9faad57
multimedia <multimedia@309-27>
2016-04-25 17:17:41 +0900

    sub (기능 +test) 추가
    뺄셈을 위해

--- src/main/java/seNex/Calculator.java
+++ src/main/java/seNex/Calculator.java
@@ -1,15 +1,19 @@
 package seNex;
 
 public class Calculator {
-	
+
 	private int result;
-	
+
 	public void add(int x, int y) {
-		result = x+y;
+		result = x + y;
 	}
-	
+
 	public int getResult() {
 		return result;
+	}
+	
+	public void sub(int x, int y) {
+		result = x - y;
 	}
 
 }
