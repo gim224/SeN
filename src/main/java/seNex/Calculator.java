54dd594c2180f5a9ed1c55774b984121eec8b43c
multimedia <multimedia@309-27>
2016-04-25 18:30:16 +0900

    곱셈기능+test 추가

--- src/main/java/seNex/Calculator.java
+++ src/main/java/seNex/Calculator.java
@@ -1,16 +1,21 @@
 package seNex;
 
 public class Calculator {
-	
+
 	private int result;
-	
+
 	public void add(int x, int y) {
-		result = x+y;
+		result = x + y;
 	}
+
 	public void sub(int x, int y) {
-		result = x-y;
+		result = x - y;
 	}
-	
+
+	public void mul(int x, int y) {
+		result = x * y;
+	}
+
 	public int getResult() {
 		return result;
 	}
--- src/test/java/seNex/CalculatorTest.java
+++ src/test/java/seNex/CalculatorTest.java
@@ -9,15 +9,22 @@ public class CalculatorTest {
 	@Test
 	public void testAdd() {
 		Calculator cal = new Calculator();
-		cal.add(10,  20);
+		cal.add(10, 20);
 		assertEquals(30, cal.getResult());
 	}
+
 	@Test
 	public void testSub() {
 		Calculator cal = new Calculator();
-		cal.sub(10,  20);
+		cal.sub(10, 20);
 		assertEquals(-10, cal.getResult());
 	}
 
-}
+	@Test
+	public void testMul() {
+		Calculator cal = new Calculator();
+		cal.mul(10, 20);
+		assertEquals(200, cal.getResult());
+	}
 
+}
